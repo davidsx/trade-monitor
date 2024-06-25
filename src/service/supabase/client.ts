18a7e5a39476eca3030313734a4704561695014c
Davidsx <davidlau900000@gmail.com>
2024-06-25 16:52:22 +0800

    update env

--- src/service/supabase/client.ts
+++ src/service/supabase/client.ts
@@ -2,6 +2,6 @@
 
 // export const createClient = () =>
 //   createBrowserClient(
-//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+//     process.env.SUPABASE_URL!,
+//     process.env.SUPABASE_KEY!,
 //   );
