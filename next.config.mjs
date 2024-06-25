1fddf1f872ef61a7b453c5711499f768a658b520
Davidsx <davidlau900000@gmail.com>
2024-06-25 14:44:46 +0800

    add icon and manifest

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@ import withPWA from 'next-pwa';
 
 /** @type {import('next-pwa').PWAConfig} */
 const pwaConfig = {
-  dest: 'public',
+  dest: 'public', // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
 };
 
 /** @type {import('next').NextConfig} */
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,23 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@/styles/globals.scss";
+import type { Metadata, Viewport } from 'next';
+import { Inter } from 'next/font/google';
+import '@/styles/globals.scss';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: 'cover',
+};
 
 export const metadata: Metadata = {
-  title: "Trade Monitor",
-  description: "Monitor my trades",
+  title: 'Trade Monitor',
+  description: 'Monitor my trades',
+  generator: 'Next.js',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
